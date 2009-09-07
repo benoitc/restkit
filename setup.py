92b1cc6fa9e28daec71e2fd971983d404a98f259
benoitc <benoitc@e-engura.org>
2009-09-07 21:43:45 +0200

    bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ import sys
 
 setup(
     name = 'restkit',
-    version = '0.8',
+    version = '0.8.1',
     description = 'Python REST kit',
     long_description = \
 """An HTTP resource kit for Python""",
