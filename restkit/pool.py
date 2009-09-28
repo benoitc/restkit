5a3ddb54a7a1baf5d304f46cf8364cca6e5aff5f
benoitc <benoitc@e-engura.org>
2009-09-28 18:54:08 +0200

    close the connection if another exception happend.

--- restkit/pool.py
+++ restkit/pool.py
@@ -213,6 +213,9 @@ class ConnectionPool(Pool):
             response.read()
         except httplib.ResponseNotReady:
             pass
+        except:
+            connection.close()
+            
             
         if connection.sock is None:
             connection = self.create()
