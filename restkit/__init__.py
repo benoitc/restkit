8621f9b9227719791a78211c490de7aaa513d928
benoitc <bchesneau@gmail.com>
2010-02-25 15:27:28 +0100

    more options to the shortcut.

--- restkit/__init__.py
+++ restkit/__init__.py
@@ -23,7 +23,8 @@ except ImportError:
     traceback.print_exc()
     
     
-def request(url, method='GET', body=None, headers=None, pool_instance=None):
+def request(url, method='GET', body=None, headers=None, pool_instance=None, 
+        follow_redirect=False, filters=None, key_file=None, cert_file=None):
     """ Quick shortcut method to pass a request
     
     :param url: str, url string
@@ -32,7 +33,17 @@ def request(url, method='GET', body=None, headers=None, pool_instance=None):
     :param headers: dict or list of tupple, http headers
     :pool intance: instance inherited from `restkit.pool.PoolInterface`. 
     It allows you to share and reuse connections connections.
+    :param follow_redirect: boolean, by default is false. If true, 
+    if the HTTP status is 301, 302 or 303 the client will follow
+    the location.
+    :param filters: list, list of http filters. see the doc of http filters 
+    for more info
+    :param key_file: the key fle to use with ssl
+    :param cert_file: the cert file to use with ssl
+    
     """
-    http_client = HttpConnection(pool_instance=pool_instance)
+    http_client = HttpConnection(follow_redirect=follow_redirect,
+            filters=filters, key_file=key_file, cert_file=cert_file,
+            pool_instance=pool_instance)
     return http_client.request(url, method=method, body=body, 
         headers=headers)
