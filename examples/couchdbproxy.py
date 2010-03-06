d9088b4ef76d8049c48b74f4edb3f76aa30f930a
benoitc <bchesneau@gmail.com>
2010-03-06 23:49:21 +0100

    add wsgi proxy doc

---
+++ examples/couchdbproxy.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -
+#
+# This file is part of restkit released under the MIT license. 
+# See the NOTICE for more information.
+
+from webob import Request
+from restkit.pool import ConnectionPool
+from restkit.ext.wsgi_proxy import HostProxy
+
+pool = ConnectionPool(max_connections=10)
+proxy = HostProxy("http://127.0.0.1:5984", pool=pool)
+
+
+def application(environ, start_response):
+  req = Request(environ)
+  req.environ['SERVER_NAME'] = '127.0.0.1'
+  req.environ['SERVER_PORT'] = '5984'
+
+  # do smth like adding oauth headers ..
+  resp = req.get_response(proxy)
+
+  # rewrite response
+  # do auth ...
+  return resp(environ, start_response)
