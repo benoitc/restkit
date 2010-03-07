673eab7a362356ea3760cf424f0fae1d75a1619a
benoitc <bchesneau@gmail.com>
2010-03-07 01:59:37 +0100

    up example and fix typo

--- examples/couchdbproxy.py
+++ examples/couchdbproxy.py
@@ -3,6 +3,8 @@
 # This file is part of restkit released under the MIT license. 
 # See the NOTICE for more information.
 
+import urlparse
+
 from webob import Request
 from restkit.pool import ConnectionPool
 from restkit.ext.wsgi_proxy import HostProxy
@@ -12,11 +14,15 @@ proxy = HostProxy("http://127.0.0.1:5984", pool=pool)
 
 
 def application(environ, start_response):
-  req = Request(environ)
+    req = Request(environ)
+    if 'RAW_URI' in req.environ: 
+        # gunicorn so we use real path non encoded
+        u = urlparse.urlparse(req.environ['RAW_URI'])
+        req.environ['PATH_INFO'] = u.path
 
-  # do smth like adding oauth headers ..
-  resp = req.get_response(proxy)
-
-  # rewrite response
-  # do auth ...
-  return resp(environ, start_response)
+    # do smth like adding oauth headers ..
+    resp = req.get_response(proxy)
+    
+    # rewrite response
+    # do auth ...
+    return resp(environ, start_response)
