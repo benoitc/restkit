1d8ce1e935d188597faccebbfce8e973e6d099d4
benoitc <bchesneau@gmail.com>
2010-03-07 16:28:42 +0100

    improve restcli. now you can display debug info and print server headers

--- restkit/console.py
+++ restkit/console.py
@@ -3,11 +3,12 @@
 # This file is part of restkit released under the MIT license. 
 # See the NOTICE for more information.
 
+from __future__ import with_statement
 import os
 import optparse as op
 import sys
 
-from restkit import __version__, request
+from restkit import __version__, request, set_logging
 
 __usage__ = "'%prog [options] url [METHOD] [filename]'"
 
@@ -17,6 +18,12 @@ def options():
         op.make_option('-H', '--header', action='append', dest='headers',
                 help='http string header in the form of Key:Value. '+
                 'For example: "Accept: application/json" '),
+        op.make_option('-S', '--server-response', action='store_true', 
+                default=False, dest='server_response', 
+                help='print server response'),
+        op.make_option('--log-level', dest="log_level",
+                help="Log level below which to silence messages. [info]",
+                default="info"),
         op.make_option('-i', '--input', action='store', dest='input', 
                 metavar='FILE', help='the name of the file to read from'),
         op.make_option('-o', '--output', action='store', dest='output',
@@ -35,6 +42,8 @@ def main():
     
     if args_len < 1:
         return parser.error('incorrect number of arguments')
+        
+    set_logging(opts.log_level)
 
     body = None
     headers = []
@@ -74,12 +83,22 @@ def main():
                         headers=headers, follow_redirect=opts.follow_redirect)
                         
         if opts.output:
-            f = open(opts.output, 'wb')
-            for block in resp.body_file:
-                f.write(block)
-            f.close()
+            with open(opts.output, 'wb') as f:
+                if opts.log_level:
+                    f.write("Server response from %s:\n" % resp.final_url)
+                    for k, v in resp.headerslist:
+                        f.write( "%s: %s" % (k, v))
+                else:
+                    for block in resp.body_file:
+                        f.write(block)
         else:
-            print resp.body
+            if opts.log_level:
+                print "\n\033[0m\033[95mServer response from %s:\n\033[0m" % resp.final_url
+                for k, v in resp.headerslist:
+                    print "\033[94m%s\033[0m: %s" % (k, v)
+                print "\033[0m"
+            else:
+                print resp.body
         
     except Exception, e:
         sys.stderr.write("An error happened: %s" % str(e))
