a80398cd2f2b0e6d1bf388eaed8a2520ee1a2710
benoitc <bchesneau@gmail.com>
2010-07-03 06:37:43 +0200

    fix issue #14. thanks!

--- restkit/client.py
+++ restkit/client.py
@@ -71,7 +71,7 @@ class HttpResponse(object):
             
     def body_string(self, charset=None, unicode_errors="strict"):
         """ return body string, by default in bytestring """
-        if self.closed:
+        if self.closed or self.response.body.closed:
             raise AlreadyRead("The response have already been read")
         body = self.response.body.read()
         if charset is not None:
@@ -84,12 +84,13 @@ class HttpResponse(object):
         
     def body_stream(self):
         """ return full body stream """
-        if self.closed:
+        if self.closed or self.response.body.closed:
             raise AlreadyRead("The response have already been read")
         return self.response.body
         
     def close(self):
         """ release the socket """
+        self.closed = True
         self.response.body.close()
           
     @property
