a196a11fe5e510d40bfbcea720f2375dc2591ad4
benoitc <bchesneau@gmail.com>
2011-02-11 21:37:41 +0100

    bump debian package

--- restkit/__init__.py
+++ restkit/__init__.py
@@ -3,8 +3,6 @@
 # This file is part of restkit released under the MIT license. 
 # See the NOTICE for more information.
 
-
-
 version_info = (3, 1, 0)
 __version__ =  ".".join(map(str, version_info))
 
