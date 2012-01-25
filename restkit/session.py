da32dafd0a6084890ce1fcb2ed9ec7e578651557
benoitc <bchesneau@gmail.com>
2012-01-24 18:58:55 -0800

    eventually we could manage different backend. even if no one really want that.

--- restkit/session.py
+++ restkit/session.py
@@ -5,12 +5,24 @@
 
 from socketpool import ConnectionPool
 from restkit.conn import Connection
-_default_session = None
+
+
+_default_session = {}
 
 def get_session(backend_name, **options):
     global _default_session
 
     if not _default_session:
-        _default_session = ConnectionPool(factory=Connection,
+        _default_session = {}
+        pool = ConnectionPool(factory=Connection,
                 backend=backend_name, **options)
-    return _default_session
+        _default_session[backend_name] = pool
+    else:
+        if backend_name not in _default_session:
+            pool = ConnectionPool(factory=Connection,
+                backend=backend_name, **options)
+
+            _default_session[backend_name] = pool
+        else:
+            pool = _default_session.get(backend_name)
+    return pool
