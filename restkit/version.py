86136dd2108d77bfeec608f96c86c193011bcc64
benoitc <bchesneau@gmail.com>
2012-01-31 01:16:38 -0800

    bump version to 4.1.0

--- restkit/version.py
+++ restkit/version.py
@@ -3,5 +3,5 @@
 # This file is part of restkit released under the MIT license.
 # See the NOTICE for more information.
 
-version_info = (4, 0, 0)
+version_info = (4, 1, 0)
 __version__ =  ".".join(map(str, version_info))
